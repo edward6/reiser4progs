37083a18e78070c12ab2522294e17af2128317fc
namesys.com!umka <namesys.com!umka>
2002-06-06 08:52:20 +0000

    .
    (Logical change 1.623)

--- reiser4lib/libaal/print.c
+++ reiser4lib/libaal/print.c
@@ -38,29 +38,10 @@ void aal_printf(const char *format, ...) {
 	aal_memset(buff, 0, sizeof(buff));
 	
 	va_start(arg_list, format);
-	aal_vsnprintf(buff, 4096, format, arg_list);
+	aal_vsnprintf(buff, sizeof(buff), format, arg_list);
 	va_end(arg_list);
 	
 	printf_handler(buff);
-}
-
-#define CONV_RANGE 1000000000
-
-int aal_dtoa(int d, size_t n, char *a) {
-	long s;
-	char *p = a;
-		
-	aal_memset(p, 0, n);
-	
-	for (s = CONV_RANGE; s > 0; s /= 10) {
-		long v = d / s;
-		if (v > 0) {
-			if (v >= 10)
-				v = (d / s) - ((v / 10) * 10);
-			*p++ = ('0' + v);
-		}
-	}
-	return strlen(a);
 }
 
 int aal_vsnprintf(char *buff, size_t n, const char *format, va_list arg_list) {
@@ -80,16 +61,16 @@ int aal_vsnprintf(char *buff, size_t n, const char *format, va_list arg_list) {
 				
 				if (*(fmt + 1) == 's') {
 					char *s = va_arg(arg_list, char *);
-					aal_strcat(buff, s);
+					aal_strncat(buff, s, n - strlen(buff));
 				}
 				if (*(fmt + 1) == 'd') {
 					int d;
-					char s[10];
+					char s[11];
 
-					aal_memset(s, 0, 10);
+					aal_memset(s, 0, sizeof(s));
 					d = va_arg(arg_list, int);
-					aal_dtoa(d, 10, s);
-					aal_strcat(buff, s);
+					aal_ltos(d, sizeof(s), s, 10);
+					aal_strncat(buff, s, n - strlen(buff));
 				}
 				fmt += 2;
 				old = fmt;
--- reiser4lib/libaal/string.c
+++ reiser4lib/libaal/string.c
@@ -27,8 +27,42 @@ char *aal_strncpy(char *dest, const char *src, size_t n) {
 	return (char *)aal_memcpy((void *)dest, (const void *)src, len);
 }
 
-char *aal_strcat(char *dest, const char *src) {
-	aal_memcpy(dest + strlen(dest), src, strlen(src));
+char *aal_strncat(char *dest, const char *src, size_t n) {
+	aal_memcpy(dest + strlen(dest), src, (strlen(src) < n ? strlen(src) : n));
 	return dest;
+}
+
+int aal_ltos(long int d, size_t n, char *a, int base) {
+	long int s;
+	char *p = a;
+	long int range;
+	
+	if (base != 10 && base != 16)
+		return 0;
+	
+	range = base == 10 ? 1000000000 : 0x10000000;
+	aal_memset(p, 0, n);
+
+	if (d < 0) 
+		*p++ = '-';
+	
+	if (base == 16) {
+		aal_strncat(p, "0x", 2);
+		p += 2;
+	}
+	
+	for (s = range; s > 0; s /= base) {
+		long int v = d / s;
+		
+		if (p - a >= n) 
+			break;
+		
+		if (v > 0) {
+			if (v >= base)
+				v = (d / s) - ((v / base) * base);
+			*p++ = ('0' + (base == 16 && v > 9 ? 39 : 0) + v);
+		}
+	}
+	return p - a;
 }
 
