c0b76a15a755668026bd0dec4df733c3697bdba0
namesys.com!umka <namesys.com!umka>
2002-10-17 17:46:20 +0000

    (Logical change 1.2441)

---
+++ plugin/sdext_lw/sdext_lw.c
@@ -0,0 +1,82 @@
+/*
+    sdext_lw.c -- light weight stat data extention plugin, that implements base
+    stat data fields.
+    
+    Copyright 1996-2002 (C) Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#include "sdext_lw.h"
+
+static reiser4_core_t *core = NULL;
+
+static errno_t sdext_lw_init(reiser4_body_t *body, 
+    void *hint) 
+{
+    sdext_lw_t *ext;
+    reiser4_sdext_lw_hint_t *sdext_lw;
+    
+    aal_assert("umka-1186", body != NULL, return -1);
+    aal_assert("umka-1187", hint != NULL, return -1);
+	
+    ext = (sdext_lw_t *)body;
+    sdext_lw = (reiser4_sdext_lw_hint_t *)body;
+    
+    sdext_lw_set_mode(ext, sdext_lw->mode);
+    sdext_lw_set_nlink(ext, sdext_lw->nlink);
+    sdext_lw_set_size(ext, sdext_lw->size);
+
+    return 0;
+}
+
+static errno_t sdext_lw_open(reiser4_body_t *body, 
+    void *hint) 
+{
+    sdext_lw_t *ext;
+    reiser4_sdext_lw_hint_t *sdext_lw;
+    
+    aal_assert("umka-1188", body != NULL, return -1);
+    aal_assert("umka-1189", hint != NULL, return -1);
+
+    ext = (sdext_lw_t *)body;
+    sdext_lw = (reiser4_sdext_lw_hint_t *)body;
+    
+    sdext_lw->mode = sdext_lw_get_mode(ext);
+    sdext_lw->nlink = sdext_lw_get_nlink(ext);
+    sdext_lw->size = sdext_lw_get_size(ext);
+    
+    return 0;
+}
+
+static int sdext_lw_confirm(reiser4_body_t *body) {
+    aal_assert("umka-1190", body != NULL, return -1);
+    return 0;
+}
+
+static uint16_t sdext_lw_length(void) {
+    return sizeof(sdext_lw_t);
+}
+
+static reiser4_plugin_t sdext_lw_plugin = {
+    .sdext_ops = {
+	.h = {
+	    .handle = NULL,
+	    .id = SDEXT_LW_ID,
+	    .type = SDEXT_PLUGIN_TYPE,
+	    .label = "sdext_lw",
+	    .desc = "Base stat data extention for reiserfs 4.0, ver. " VERSION,
+	},
+	.init	 = sdext_lw_init,
+	.open	 = sdext_lw_open,
+	.confirm = sdext_lw_confirm,
+	.length	 = sdext_lw_length
+    }
+};
+
+static reiser4_plugin_t *sdext_lw_start(reiser4_core_t *c) {
+    core = c;
+    return &sdext_lw_plugin;
+}
+
+plugin_register(sdext_lw_start);
+
---
+++ plugin/sdext_lw/sdext_lw.h
@@ -0,0 +1,31 @@
+/*
+    sdext_lw.h -- stat data plugin, that implements base stat data fields.
+    Copyright 1996-2002 (C) Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef SDEXT_LW_H
+#define SDEXT_LW_H
+
+#include <aal/aal.h>
+#include <reiser4/plugin.h>
+
+struct sdext_lw {
+    d16_t mode;
+    d32_t nlink;
+    d64_t size;
+};
+
+typedef struct sdext_lw sdext_lw_t;
+
+#define sdext_lw_get_mode(stat)		aal_get_le16(stat, mode)
+#define sdext_lw_set_mode(stat, val)	aal_set_le16(stat, mode, val)
+
+#define sdext_lw_get_nlink(stat)	aal_get_le32(stat, nlink)
+#define sdext_lw_set_nlink(stat, val)	aal_set_le32(stat, nlink, val)
+
+#define sdext_lw_get_size(stat)		aal_get_le64(stat, size)
+#define sdext_lw_set_size(stat, val)	aal_set_le64(stat, size, val)
+
+#endif
+
