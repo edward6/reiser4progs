87f9a822821852fd5caf558b88153788d5bebaf1
namesys.com!umka <namesys.com!umka>
2002-10-19 15:02:41 +0000

    (Logical change 1.2452)

---
+++ plugin/extent40/extent40.h
@@ -0,0 +1,27 @@
+/*
+    extent40 -- resier4 default extent plugin.
+    Copyright (C) 1996-2002 Hans Reiser.
+    Author Yury Umanets.
+*/
+
+#ifndef EXTENT40
+#define EXTENT40
+
+#include <aal/aal.h>
+#include <reiser4/plugin.h>
+
+struct extent40 {
+    blk_t start;
+    count_t width;
+};
+
+typedef struct extent40 extent40_t;
+
+#define et40_get_start(et)	    aal_get_le64(et, start)
+#define et40_set_start(et, val)	    aal_set_le64(et, start, val)
+
+#define et40_get_width(et)	    aal_get_le64(et, width)
+#define et40_set_width(et, val)	    aal_set_le64(et, width, val)
+
+#endif
+
