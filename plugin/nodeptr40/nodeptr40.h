d039fdf48be716381333b0b616e96fca7727c620
namesys.com!umka <namesys.com!umka>
2002-11-06 22:54:09 +0000

    internal40 plugin renamed to nodeptr40.
    2002/11/07 01:35:53+03:00 namesys.com!umka
    Rename: plugin/nodeptr40/internal40.h -> plugin/nodeptr40/nodeptr40.h
    
    (Logical change 1.2492)

---
+++ plugin/nodeptr40/nodeptr40.h
@@ -0,0 +1,26 @@
+/*
+    nodeptr40.h -- reiser4 dafault internal item structures.
+    Copyright (C) 1996-2002 Hans Reiser.
+*/
+
+#ifndef NODEPTR40_H
+#define NODEPTR40_H
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <aal/aal.h>
+#include <reiser4/plugin.h>
+
+struct nodeptr40 {
+    d64_t ptr;
+};
+
+typedef struct nodeptr40 nodeptr40_t;
+
+#define np40_get_ptr(np)	aal_get_le64(np, ptr)
+#define np40_set_ptr(np, val)	aal_set_le64(np, ptr, val)
+
+#endif
+
