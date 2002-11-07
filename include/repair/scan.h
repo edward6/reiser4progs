ed71f53c27d9f71e774c513d0478dcfce75267dc
namesys.com!vitaly <namesys.com!vitaly>
2002-11-07 18:43:41 +0000

    2002/11/07 19:16:03+03:00 namesys.com!vitaly
    (Logical change 1.2495)

---
+++ include/repair/scan.h
@@ -0,0 +1,17 @@
+/*
+    repair/scan.h -- common structures and methods for fsck pass2.
+    Copyright 1996-2002 (C) Hans Reiser.
+*/
+
+#ifndef REPAIR_SCAN_H
+#define REPAIR_SCAN_H
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <repair/repair.h>
+
+errno_t repair_scan_node_check(reiser4_node_t *node, void *data);
+
+#endif
