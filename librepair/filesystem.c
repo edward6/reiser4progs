3c7bfe1262f682d55a3cf8db8b66d0fd3bd674fc
namesys.com!vitaly <namesys.com!vitaly>
2002-11-07 18:43:41 +0000

    Auto merged
    2002/11/07 21:26:30+03:00 namesys.com!vitaly
    
    2002/11/07 17:55:05+03:00 namesys.com!vitaly
    
    (Logical change 1.2495)

--- librepair/filesystem.c
+++ librepair/filesystem.c
@@ -6,188 +6,10 @@
 
 #include <repair/librepair.h>
 
-static reiser4_node_t *__node_open(aal_block_t *block, void *data) {
-    return reiser4_node_open(block);
-}
-
-static errno_t __before_traverse(reiser4_node_t *node, reiser4_item_t *item, 
-    void *data) 
-{
-    repair_check_t *check_data = data;
-    
-    aal_assert("vpf-251", node != NULL, return -1);
-    aal_assert("vpf-253", check_data != NULL, return -1);
-    aal_assert("vpf-254", check_data->format != NULL, return -1);
-
-    /* Initialize the level for the root node before traverse. */
-    if (!repair_cut_data(check_data)->level)
-	repair_cut_data(check_data)->level = 
-	    (node->entity->plugin->node_ops.get_level ? 
-	    node->entity->plugin->node_ops.get_level(node->entity) : 
-	    reiser4_format_get_height(check_data->format));
-    
-    repair_cut_data(check_data)->level--;
-
-    repair_cut_data(check_data)->nodes_path = 
-	aal_list_append(repair_cut_data(check_data)->nodes_path, node);
-    repair_cut_data(check_data)->items_path = 
-	aal_list_append(repair_cut_data(check_data)->items_path, item);
-
-    return 0;
-}
-
-static errno_t __after_traverse(reiser4_node_t *node, reiser4_item_t *item, 
-    void *data) 
-{
-    repair_check_t *check_data = data;
-    
-    aal_assert("vpf-256", check_data != NULL, return -1);
-    
-    repair_cut_data(check_data)->level++;
-
-    if (reiser4_node_count(node) == 0)
-	repair_set_flag(check_data, REPAIR_NOT_FIXED);
-
-    aal_list_remove(repair_cut_data(check_data)->nodes_path, 
-	aal_list_last(repair_cut_data(check_data)->nodes_path)->data);
-    aal_list_remove(repair_cut_data(check_data)->items_path, 
-	aal_list_last(repair_cut_data(check_data)->items_path)->data);
-    
-    return 0;
-}
-
-static errno_t __setup_traverse(reiser4_node_t *node, reiser4_item_t *item, 
-    void *data)
-{
-    repair_check_t *check_data = data;
-    blk_t target;
-    int res;
-    
-    aal_assert("vpf-255", data != NULL, return -1);
-    aal_assert("vpf-269", item != NULL, return -1);
-
-    if (repair_item_nptr_check(item, check_data))
-	return -1;
-
-    return 0;
-}
-
-static errno_t __update_traverse(reiser4_node_t *node, reiser4_item_t *item, 
-    void *data) 
-{
-    repair_check_t *check_data = data;
-    
-    aal_assert("vpf-257", check_data != NULL, return -1);
-    
-    if (repair_test_flag(check_data, REPAIR_NOT_FIXED)) {
-	/* The node corruption was not fixed - delete the internal item. */
-	if (reiser4_node_remove(node, item->pos)) {
-	    if (item->pos->unit == ~0ul)
-		aal_exception_error("Node (%llu): Failed to remove the item "
-		    "(%u).", aal_block_number(node->block), item->pos->item);
-	    else
-		aal_exception_error("Node (%llu): Failed to remove the unit "
-		    "(%u) from  item (%u).", aal_block_number(node->block), 
-		    item->pos->item, item->pos->unit);
-	    return -1;
-	}
-	if (item->pos->unit == ~0ul) 
-	    item->pos->item--;
-	else 
-	    item->pos->unit--;	
-	repair_clear_flag(check_data, REPAIR_NOT_FIXED);
-    }
-    
-    return 0;
-}
-
-static errno_t __node_check(reiser4_node_t *node, void *data) {
-    repair_check_t *check_data = data;
-    errno_t res;
-    
-    aal_assert("vpf-252", check_data != NULL, return -1);
-    
-    if ((res = repair_node_check(node, check_data)) > 0) 
-	repair_set_flag(check_data, REPAIR_NOT_FIXED);
-    else 
-	return res;
-        
-    return 0;
-}
-
-static errno_t repair_fs_check_setup(reiser4_fs_t *fs, repair_check_t *data) {
-    /* Prepare a control allocator */
-    
-/*  Will be needed on scan pass.
-    if (!(data->a_control = reiser4_alloc_create(fs->format, 
-	reiser4_format_get_len(fs->format)))) 
-    {
-	aal_exception_fatal("Failed to create a control allocator.");
-	return -1;
-    }
-        
-    if (!(data->oid_control = reiser4_oid_create(fs->format))) {
-	aal_exception_fatal("Failed to create a control oid allocator.");
-	return -1;
-    }
-    
-   // Mark the format area as used in the control allocator
-    reiser4_format_mark(fs->format, data->a_control);
-*/
- 
-    if (!(repair_cut_data(data)->once_pointed = aux_bitmap_create(
-	reiser4_format_get_len(data->format)))) 
-    {
-	aal_exception_error("Failed to allocate a bitmap for once pointed blocks.");
-	return -1;
-    }
-    
- 
-    if (!(repair_cut_data(data)->many_pointed = aux_bitmap_create(
-	reiser4_format_get_len(data->format)))) 
-    {
-	aal_exception_error("Failed to allocate a bitmap for many pointed blocks.");
-	return -1;
-    }
-
-    if (!(repair_cut_data(data)->format_layout = aux_bitmap_create(
-	reiser4_format_get_len(data->format)))) 
-    {
-	aal_exception_error("Failed to allocate a bitmap for format layout blocks.");
-	return -1;
-    }
-    
-    aal_memset(repair_cut_data(data)->format_layout->map, 0xff, 
-	repair_cut_data(data)->format_layout->size);
-    
-    if (reiser4_format_layout(fs->format, callback_mark_format_block, 
-	repair_cut_data(data)->format_layout)) 
-    {
-	aal_exception_error("Failed to mark all format blocks in the bitmap as unused.");
-	return -1;
-    }
-    
-    data->format = fs->format;
-    data->options = repair_data(fs)->options;
-
-    return 0;
-}
-
-static errno_t repair_fs_check_update(reiser4_fs_t *fs, repair_check_t *data) {
-    
-    if (repair_test_flag(data, REPAIR_NOT_FIXED)) {
-	reiser4_format_set_root(data->format, ~0ull);
-	repair_clear_flag(data, REPAIR_NOT_FIXED);
-    }
-
-    return 0;
-}
-
 errno_t repair_fs_check(reiser4_fs_t *fs) {
     repair_check_t data;
     blk_t blk;
-    aal_block_t *block;
-    int res;    
+    errno_t res;    
 
     aal_assert("vpf-180", fs != NULL, return -1);
     aal_assert("vpf-181", fs->format != NULL, return -1);
@@ -196,29 +18,25 @@ errno_t repair_fs_check(reiser4_fs_t *fs) {
     aal_memset(&data, 0, sizeof(data));
     
     blk = reiser4_format_get_root(fs->format);
-    if ((res = reiser4_format_layout(fs->format, callback_data_block_check, &blk)) &&
-	res != -1) 
-    {
-	aal_exception_error("Bad root block (%llu). (A previous recovery did "
-	    "not complete probably).", blk);
-	return 0;
-    }
- 
-    if (!(block = aal_block_open(fs->format->device, blk))) {
-	aal_exception_error("Can't read block %llu. %s.", blk, 
-	    fs->format->device->error);
-	return -1;
-    }
     
-    if (repair_fs_check_setup(fs, &data))
+    if (repair_cut_tree_setup(fs, &data))
 	return -1;    
-    
-    reiser4_node_traverse(block, __node_open, __node_check, __before_traverse, 
-	__setup_traverse, __update_traverse, __after_traverse, &data);
+   
+    /* Cut the corrupted, unrecoverable parts of the tree off. */ 
+    if ((res = reiser4_node_traverse(blk, repair_cut_tree_node_open, 
+	repair_cut_tree_node_check, repair_cut_tree_before_traverse, 
+	repair_cut_tree_setup_traverse, repair_cut_tree_update_traverse, 
+	repair_cut_tree_after_traverse, &data)) < 0)
+	return res;
 
-    if (repair_fs_check_update(fs, &data))
+    if (repair_cut_tree_update(fs, &data))
 	return -1;
 
+    /* Solve overlapped problem within the tree. */
+    if ((res = reiser4_node_traverse(blk, repair_node_open, 
+	repair_scan_node_check, NULL, NULL, NULL, NULL, &data)) < 0)
+	return res;
+  
     return 0;
 }
 
