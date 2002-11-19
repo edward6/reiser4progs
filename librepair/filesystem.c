680c6786a8196ac4d67db4567c23d74c701b542a
namesys.com!vitaly <namesys.com!vitaly>
2002-11-19 17:13:40 +0000

    merge
    2002/11/19 20:05:42+03:00 namesys.com!vitaly
    
    2002/11/19 18:42:18+03:00 namesys.com!vitaly
    
    (Logical change 1.2571)

--- librepair/filesystem.c
+++ librepair/filesystem.c
@@ -25,10 +25,8 @@ errno_t repair_fs_check(reiser4_fs_t *fs) {
     {
 	repair_set_flag(&data, REPAIR_NOT_FIXED);
     } else {
-	traverse_hint_t hint = {TO_BACKWARD, LEAF_LEVEL};
-	    
 	/* Cut the corrupted, unrecoverable parts of the tree off. */ 
-	if ((res = reiser4_joint_traverse(joint, &hint, &data, 
+	if ((res = reiser4_joint_traverse(joint, &data, 
 	    repair_filter_joint_open,      repair_filter_joint_check, 
 	    repair_filter_before_traverse, repair_filter_setup_traverse, 
 	    repair_filter_update_traverse, repair_filter_after_traverse)) < 0)
@@ -39,91 +37,16 @@ errno_t repair_fs_check(reiser4_fs_t *fs) {
 	return -1;
 
     if (reiser4_format_get_root(fs->format) == FAKE_BLK) {
-	traverse_hint_t hint = {TO_BACKWARD, LEAF_LEVEL};
-	    
 	if (repair_scan_setup(fs, &data))
 	    return -1;
-	
+
 	/* Solve overlapped problem within the tree. */
-	if ((res = reiser4_joint_traverse(joint, &hint, &data,
+	if ((res = reiser4_joint_traverse(joint, &data,
 	    repair_filter_joint_open, repair_scan_node_check,
 	    NULL, NULL, NULL, NULL)) < 0)
 	    return res;
     }
       
-    return 0;
-}
-
-static errno_t repair_master_check(reiser4_fs_t *fs, 
-    callback_ask_user_t ask_blocksize) 
-{
-    uint16_t blocksize = 0;
-    int error = 0;
-    reiser4_plugin_t *plugin;
-   
-    aal_assert("vpf-161", fs != NULL, return -1);
-    aal_assert("vpf-163", ask_blocksize != NULL, return -1);
-    aal_assert("vpf-164", repair_data(fs) != NULL, return -1);
-    aal_assert("vpf-170", repair_data(fs)->host_device != NULL, return -1);
-    
-    if (!fs->master) {
-	/* Master SB was not opened. Create a new one. */
-	if (aal_exception_throw(EXCEPTION_FATAL, EXCEPTION_YESNO, 
-	    "Master super block cannot be found. Do you want to build a new "
-	    "one on (%s)?", aal_device_name(repair_data(fs)->host_device)) == 
-	    EXCEPTION_NO) 
-	    return -1;
-
-        if (!(blocksize = ask_blocksize(fs, &error)) && error)
-	    return -1;
-
-	/* 
-	    FIXME-VITALY: What should be done here with uuid and label? 
-	    At least not here as uiud and label seem to be on the wrong place.
-	    Move them to specific SB.
-	*/
-	
-	/* Create a new master SB. */
-	if (!(fs->master = reiser4_master_create(repair_data(fs)->host_device, 
-	    FAKE_PLUGIN, blocksize, NULL, NULL))) 
-	{
-	    aal_exception_fatal("Cannot create a new master super block.");
-	    return -1;
-	} else if (repair_verbose(repair_data(fs)))
-	    aal_exception_info("A new master superblock was created on (%s).", 
-		aal_device_name(repair_data(fs)->host_device));
-    } else {
-	/* Master SB was opened. Check it for validness. */
-
-	/* Check the blocksize. */
-	if (!aal_pow_of_two(reiser4_master_blocksize(fs->master))) {
-	    aal_exception_fatal("Invalid blocksize found in the master super "
-		"block (%u).", reiser4_master_blocksize(fs->master));
-	    
-	    if (!(blocksize = ask_blocksize(fs, &error)) && error)
-		return -1;
-
-	    set_mr_blocksize(fs->master->super, blocksize);
-	} 
-    }
-
-    /* Setting actual used block size from master super block */
-    if (aal_device_set_bs(repair_data(fs)->host_device, 
-	reiser4_master_blocksize(fs->master))) 
-    {
-        aal_exception_fatal("Invalid block size was specified (%u). It must "
-	    "be power of two.", reiser4_master_blocksize(fs->master));
-	return -1;
-    }
-    
-    return 0;
-}    
-
-static errno_t repair_alloc_check(reiser4_fs_t *fs) {
-    return 0;
-}
-
-static errno_t repair_oid_check(reiser4_fs_t *fs) {
     return 0;
 }
 
@@ -157,10 +80,7 @@ reiser4_fs_t *repair_fs_open(repair_data_t *data,
     if (repair_format_check(fs))
 	goto error_free_format;
     
-    fs->alloc = reiser4_alloc_open(fs->format, 
-	reiser4_format_get_len(fs->format));
-    
-    if (repair_alloc_check(fs))
+    if (repair_alloc_open(fs))
 	goto error_free_alloc;
 
     /* Initializes oid allocator */
