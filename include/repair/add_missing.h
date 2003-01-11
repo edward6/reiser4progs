37404273e2998962dd1db8600c6e6f5c363d986b
namesys.com!vitaly <namesys.com!vitaly>
2003-01-11 17:58:34 +0000

    2002/12/19 23:37:46+03:00 namesys.com!vitaly
    2002/12/19 23:07:05+03:00 namesys.com!vitaly
    
    (Logical change 1.2737)

---
+++ include/repair/add_missing.h
@@ -0,0 +1,17 @@
+/*
+    repair/add_missing.h -- the common structures and methods for insertion leaves
+    and extent item from twigs unconnected from the tree.
+    Copyright (C) 1996 - 2002 Hans Reiser
+*/
+
+#ifndef ADD_MISSING_H
+#define ADD_MISSING_H
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+extern errno_t repair_am_pass(repair_data_t *);
+
+#endif
+
