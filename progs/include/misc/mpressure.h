55147a4e0bbf3069664aea34733e8a62ac308894
namesys.com!umka <namesys.com!umka>
2003-02-18 10:12:11 +0000

    (Logical change 1.2897)

---
+++ progs/include/misc/mpressure.h
@@ -0,0 +1,9 @@
+/*
+  mpressure.c -- memory pressure detect functions common for all reiser4progs.
+  Probably here should be more reliable method to determine memory pressure.
+    
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+extern bool_t progs_mpressure_detect(void);
