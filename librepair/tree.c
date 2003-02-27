2a8fe28fc9c6f008e161e9815c6437b2ceea822e
namesys.com!vitaly <namesys.com!vitaly>
2003-02-27 17:52:22 +0000

    Auto merged
    2003/02/27 20:51:42+03:00 namesys.com!vitaly
    
    (Logical change 1.2921)

--- librepair/tree.c
+++ librepair/tree.c
@@ -1,7 +1,7 @@
 /*
     tree.c -- repair/tree.c -- tree auxiliary code.
   
-    Copyright (C) 2001, 2002 by Hans Reiser, licensing governed by
+    Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
     reiser4progs/COPYING.
 */
 
