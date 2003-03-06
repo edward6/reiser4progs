e20bdc9d81708d27f78ad86d3f450f289f028802
namesys.com!umka <namesys.com!umka>
2003-03-06 16:50:26 +0000

    Cleanups in debugfs. All code splitted in to standalone fileds related to it by topic
    2003/03/06 18:29:46+03:00 namesys.com!umka
    
    (Logical change 1.2946)

---
+++ progs/debugfs/print.h
@@ -0,0 +1,33 @@
+/*
+  print.h -- filesystem print related functions declaration.
+
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+#ifndef DEBUGFS_PRINT_H
+#define DEBUGFS_PRINT_H
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h> 
+#endif
+
+#include <reiser4/reiser4.h>
+
+extern errno_t debugfs_print_block(reiser4_fs_t *fs,
+				   blk_t blk);
+
+extern errno_t debugfs_print_file(reiser4_fs_t *fs,
+				  char *filename,
+				  uint32_t flags);
+
+extern errno_t debugfs_print_node(reiser4_node_t *node);
+
+extern errno_t debugfs_print_tree(reiser4_fs_t *fs);
+extern errno_t debugfs_print_master(reiser4_fs_t *fs);
+extern errno_t debugfs_print_format(reiser4_fs_t *fs);
+extern errno_t debugfs_print_oid(reiser4_fs_t *fs);
+extern errno_t debugfs_print_alloc(reiser4_fs_t *fs);
+extern errno_t debugfs_print_journal(reiser4_fs_t *fs);
+
+#endif
