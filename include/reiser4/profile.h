92a4b883deeffbebd026c81a96d08119aa018b45
namesys.com!umka <namesys.com!umka>
2003-03-07 17:25:26 +0000

    Rewritten profiles in libmisc. A lot of cleanups and chenges in mkfs and debugfs. Fixes in mkfs manual. Added override profile ability to mkfs.c
    2003/03/07 18:40:24+03:00 namesys.com!umka
    
    (Logical change 1.2949)

---
+++ include/reiser4/profile.h
@@ -0,0 +1,19 @@
+/*
+  profile.h -- reiser4 profile functions.
+  
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+#ifndef REISER4_PROFILE_H
+#define REISER4_PROFILE_H
+
+extern uint64_t reiser4_profile_value(reiser4_profile_t *profile, 
+				      const char *name); 
+
+extern reiser4_pid_t *reiser4_profile_pid(reiser4_profile_t *profile,
+					  const char *name);
+
+extern errno_t reiser4_profile_override(reiser4_profile_t *profile, 
+					const char *type, const char *name);
+#endif
