950b7ac6c840ba2e75254a5d2ef053d547bd1af2
namesys.com!umka <namesys.com!umka>
2003-03-18 16:58:48 +0000

    (Logical change 1.3007)

---
+++ include/reiser4/print.h
@@ -0,0 +1,18 @@
+/*
+  print.h -- handlers for expanded printf keys like %k for printing
+  reiser4_key_t and so on.
+  
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#ifndef REISER4_PRINT_H
+#define REISER4_PRINT_H
+
+extern errno_t reiser4_print_init(void);
+
+#endif
