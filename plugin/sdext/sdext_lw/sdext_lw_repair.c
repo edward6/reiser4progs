d7436957698fa61b71c7d3f6a94de63bebfebd7f
namesys.com!vitaly <namesys.com!vitaly>
2003-03-21 21:32:07 +0000

    2003/03/21 23:17:08+03:00 namesys.com!vitaly
    (Logical change 1.3023)

---
+++ plugin/sdext/sdext_lw/sdext_lw_repair.c
@@ -0,0 +1,32 @@
+/*
+  sdext_lw_repair.c -- light weight stat data extention plugin recovery code.
+    
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#ifndef ENABLE_ALONE
+
+#include "sdext_lw.h"
+#include <repair/repair_plugins.h>
+
+errno_t sdext_lw_check(sdext_entity_t *sdext, uint8_t mode) {
+    aal_assert("vpf-777", sdext != NULL);
+    aal_assert("vpf-783", sdext->plugin != NULL);
+
+    if (sdext->pos + sizeof(sdext_lw_t) > sdext->len) {
+	aal_exception_error("Does not look like a valid (%s) statdata "
+	    "extention.", sdext->plugin->h.label);
+
+	return 	REPAIR_FATAL;
+    }
+    
+    return REPAIR_OK;
+}
+
+#endif
+
