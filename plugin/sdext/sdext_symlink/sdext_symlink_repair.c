a3ef97c93c4972c35b046aee57e8cd71c5bf068b
namesys.com!vitaly <namesys.com!vitaly>
2003-03-21 21:32:07 +0000

    2003/03/21 23:17:39+03:00 namesys.com!vitaly
    (Logical change 1.3023)

---
+++ plugin/sdext/sdext_symlink/sdext_symlink_repair.c
@@ -0,0 +1,39 @@
+/*
+  sdext_symlink.c -- symlink stat data extention plugin.
+    
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#ifndef ENABLE_ALONE
+
+#include <reiser4/plugin.h>
+#include <repair/repair_plugins.h>
+
+errno_t sdext_symlink_check(sdext_entity_t *sdext, uint8_t mode) {
+    uint32_t pos;
+    
+    aal_assert("vpf-779", sdext != NULL);
+    aal_assert("vpf-780", sdext->plugin != NULL);
+    
+    pos = sdext->pos;
+    
+    while (*((char *)sdext->body + pos)) {
+	pos++;
+	if (pos == sdext->len) {
+	    aal_exception_error("Does not look like a valid (%s) statdata "
+		"extention.", sdext->plugin->h.label);
+	    
+	    return REPAIR_FATAL;
+	}
+    }
+    
+    return REPAIR_OK;
+}
+
+#endif
+
