f3117064c50e62618cc574904c5bdf389cc395b8
namesys.com!vitaly <namesys.com!vitaly>
2003-03-21 21:32:07 +0000

    2003/03/21 23:17:27+03:00 namesys.com!vitaly
    (Logical change 1.3023)

---
+++ plugin/sdext/sdext_lt/sdext_lt_repair.c
@@ -0,0 +1,32 @@
+/*
+  sdext_lt_repair.c -- large time stat data extention plugin recovery code.
+    
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#ifndef ENABLE_ALONE
+
+#include "sdext_lt.h"
+#include "repair/repair_plugins.h"
+
+errno_t sdext_lt_check(sdext_entity_t *sdext, uint8_t mode) {
+    aal_assert("vpf-776", sdext != NULL);
+    aal_assert("vpf-782", sdext->plugin != NULL);
+
+    if (sdext->pos + sizeof(sdext_lt_t) > sdext->len) {
+	aal_exception_error("Does not look like a valid (%s) statdata "
+	    "extention.", sdext->plugin->h.label);
+	return REPAIR_FATAL;
+    }
+    
+    return REPAIR_OK;
+}
+
+#endif
+
