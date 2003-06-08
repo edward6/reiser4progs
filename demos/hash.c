2951cdffb96705c771d1b32212a9fc36eba6c03d
namesys.com!umka <namesys.com!umka>
2003-06-08 16:16:49 +0000

    Added ./demos/hash.c for testing libaal hash table functions.
    2003/06/08 20:01:15+04:00 namesys.com!umka
    
    (Logical change 1.3332)

---
+++ demos/hash.c
@@ -0,0 +1,46 @@
+/*
+  hash.c -- small hash table using example.
+
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+#include <stdio.h>
+#include <aal/aal.h>
+
+static uint32_t hash_func(const void *key) {
+	return (uint32_t)key;
+}
+
+static int comp_func(const void *key1,
+		     const void *key2,
+		     void *data)
+{
+	uint32_t k1 = (uint32_t)key1;
+	uint32_t k2 = (uint32_t)key2;
+
+	if (k1 < k2)
+		return -1;
+
+	if (k1 > k2)
+		return 1;
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	uint32_t i;
+	aal_hash_table_t *table;
+
+	if (!(table = aal_hash_table_alloc(hash_func, comp_func)))
+		return -1;
+
+	for (i = 1; i <= 100; i++) {
+		aal_hash_table_insert(table, (void *)i,
+				      (void *)(1 * 100));
+	}
+
+	aal_hash_table_free(table);
+	return 0;
+}
+
