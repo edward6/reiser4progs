c212ce0c81978a0513b4e01e9389f8cb8997ecd3
namesys.com!umka <namesys.com!umka>
2003-07-14 13:37:04 +0000

    Lots of cleanups and fixes in tree.c. Changes in flush and memory pressure handling. Changes in code style in progs/*
    2003/07/14 16:44:59+04:00 namesys.com!umka
    
    (Logical change 1.3423)

---
+++ include/reiser4/fake.h
@@ -0,0 +1,21 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   fake.h -- functions for working with fake block numbers. */
+
+#ifndef REISER4_FAKE_H
+#define REISER4_FAKE_H
+
+#ifndef ENABLE_STAND_ALONE
+
+#include <reiser4/types.h>
+
+#define REISER4_FAKE_BLOCKNR_VAL   0xf000000000000000ull
+#define REISER4_FAKE_BLOCKNR_MASK  0x8000000000000000ull
+
+extern inline blk_t reiser4_fake_get(void);
+extern inline int reiser4_fake_ack(blk_t blk);
+
+#endif
+
+#endif
