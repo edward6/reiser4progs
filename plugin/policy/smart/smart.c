530f8d948905cb3bb29241b245c95a1d039ceeb8
namesys.com!umka <namesys.com!umka>
2003-07-28 13:41:38 +0000

    Added tail policy plugins. Lots of chnages in profile using and handling.
    2003/07/28 16:27:28+04:00 namesys.com!umka
    
    (Logical change 1.3447)

---
+++ plugin/policy/smart/smart.c
@@ -0,0 +1,35 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   smart.c -- smart tail policy plugin. */
+
+#ifndef ENABLE_STAND_ALONE
+#include <reiser4/plugin.h>
+
+static int smart_tails(uint64_t value) {
+	if (value > 16384)
+		return 0;
+	
+	return 1;
+}
+
+reiser4_policy_ops_t smart_ops = {
+	.tails = smart_tails
+};
+
+static reiser4_plugin_t smart_plugin = {
+	.cl    = CLASS_INIT,
+	.id    = {TAIL_SMART_ID, 0, POLICY_PLUGIN_TYPE},
+	.label = "smart",
+	.desc  = "Smart tail policy for reiser4, ver. " VERSION,
+	.o = {
+		.policy_ops = &smart_ops
+	}
+};
+
+static reiser4_plugin_t *smart_start(reiser4_core_t *c) {
+	return &smart_plugin;
+}
+
+plugin_register(smart, smart_start, NULL);
+#endif
