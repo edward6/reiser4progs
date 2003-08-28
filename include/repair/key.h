ac7b08db1d56afb1433f8e5abf0229327f40d134
namesys.com!vitaly <namesys.com!vitaly>
2003-08-28 15:29:15 +0000

    (Logical change 1.3590)

---
+++ include/repair/key.h
@@ -0,0 +1,13 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   key.h -- reiser4 key defines and functions. */
+
+#ifndef REPAIR_KEY_H
+#define REPAIR_KEY_H
+
+#include <reiser4/types.h>
+
+extern errno_t repair_key_check_struct(reiser4_key_t *key);
+
+#endif
