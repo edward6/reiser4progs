1b29628a51c0b7e49786e188a04d784b54aab374
namesys.com!vitaly <namesys.com!vitaly>
2003-08-28 15:37:05 +0000

    (Logical change 1.3591)

---
+++ librepair/key.c
@@ -0,0 +1,12 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   key.c -- repair key code. */ 
+
+#include <reiser4/reiser4.h>
+
+errno_t repair_key_check_struct(reiser4_key_t *key) {
+	aal_assert("vpf-1279", key != NULL);
+
+	return plug_call(key->plug->o.key_ops, check_struct, key);
+}
