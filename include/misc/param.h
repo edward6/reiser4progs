248db49e65445539d77f458d4643d75893054ee9
namesys.com!umka <namesys.com!umka>
2003-09-03 14:04:17 +0000

    Profile renames to params everywher in codde in manual pages. Fixes related to stand alone mode.
    2003/09/03 17:18:02+04:00 namesys.com!umka
    Rename: include/misc/profile.h -> include/misc/param.h
    
    (Logical change 1.3627)

---
+++ include/misc/param.h
@@ -0,0 +1,14 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   param.h -- methods for working with params reiser4
+   programs. */
+
+#ifndef MISC_PARAM_H
+#define MISC_PARAM_H
+
+#include <reiser4/types.h>
+
+extern void misc_param_print(void);
+extern errno_t misc_param_override(char *override);
+#endif
