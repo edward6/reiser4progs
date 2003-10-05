ea04c57a5b07af69f42da9b5c633164ef6ff262f
namesys.com!umka <namesys.com!umka>
2003-10-05 14:54:53 +0000

    Added special file plugin. Suuport it in debugfs. Lots of fixes and cleanups in object plugins.
    2003/10/05 14:41:16+04:00 namesys.com!umka
    
    (Logical change 1.3731)

---
+++ plugin/object/spl40/spl40.h
@@ -0,0 +1,28 @@
+/*
+  spl40.h -- reiser4 special file plugin structures.
+
+  Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+  reiser4progs/COPYING.
+*/
+
+#ifndef SPL40_H
+#define SPL40_H
+
+#include <aal/aal.h>
+#include <aux/aux.h>
+#include <sys/stat.h>
+
+#include <reiser4/plugin.h>
+#include <plugin/object/obj40/obj40.h>
+
+/* Special file struct. */
+struct spl40 {
+
+	/* Common file fiedls (statdata, etc). As spl40 has nothing but statdata
+	   only, this structure has only file handler, which contains stuff for
+	   statdata handling. */
+	obj40_t obj;
+};
+
+typedef struct spl40 spl40_t;
+#endif
