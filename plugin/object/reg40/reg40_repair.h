5aeac1a4be6c5f894c2cc4da74c04235f6421637
namesys.com!umka <namesys.com!umka>
2003-10-07 13:16:34 +0000

    Lots of cleanups and comments in object plugins. As special file plugin added, lots of bugs fixed, we change version to 0.4.17.
    2003/10/07 15:27:09+04:00 namesys.com!umka
    
    (Logical change 1.3737)

---
+++ plugin/object/reg40/reg40_repair.h
@@ -0,0 +1,20 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   reg40_repair.h -- reiser4 regular file plugin repair functions. */
+
+#ifndef REG40_REPAIR_H
+#define REG40_REPAIR_H
+
+#include <aal/aal.h>
+#include <reiser4/plugin.h>
+
+extern errno_t reg40_form(object_entity_t *object);
+
+extern errno_t reg40_check_struct(object_entity_t *object,
+				  place_func_t place_func,
+				  void *data, uint8_t mode);
+
+extern object_entity_t *reg40_recognize(object_info_t *info);
+#endif
+
