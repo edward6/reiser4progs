3a2cc558c7a3e2410f5286e95f8f84985de9c7cb
namesys.com!umka <namesys.com!umka>
2003-10-08 12:41:23 +0000

    Comments and cleanups in sym40. Refactored path resolving. It is moved now from obejct.c to semantic.c. Comments in semantic.c and object.c. Fixes in object.c.
    2003/10/08 13:09:08+04:00 namesys.com!umka
    
    (Logical change 1.3738)

---
+++ include/reiser4/semantic.h
@@ -0,0 +1,18 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   semantic.h -- reiser4 semantic tree functions. */
+
+#ifndef REISER4_SEMANTIC_H
+#define REISER4_SEMANTIC_H
+
+#include <reiser4/types.h>
+
+extern reiser4_plug_t *reiser4_semantic_plug(reiser4_tree_t *tree,
+					     reiser4_place_t *place);
+
+extern object_entity_t *reiser4_semantic_resolve(reiser4_tree_t *tree,
+						 char *path,
+						 reiser4_key_t *from,
+						 bool_t follow);
+#endif
