834f01d316f1564b02610fd4a2768ee7e31185d1
namesys.com!umka <namesys.com!umka>
2003-10-11 19:10:14 +0000

    Chnages in stat40_insert(), stat40_update(), added stat40_remove(). Lots of fixes and cleanups in all plugins.
    2003/10/11 22:09:54+04:00 namesys.com!umka
    
    (Logical change 1.3747)

---
+++ plugin/alloc/alloc40/alloc40_repair.h
@@ -0,0 +1,21 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   alloc40_repair.h -- reiser4 block allocator plugin repair functions. */
+
+#ifndef ALLOC40_REPAIR_H
+#define ALLOC40_REPAIR_H
+
+#include <aal/aal.h>
+#include <reiser4/plugin.h>
+
+extern errno_t alloc40_check_struct(generic_entity_t *entity,
+				    uint8_t mode);
+
+extern errno_t alloc40_region(generic_entity_t *entity, blk_t blk, 
+			      region_func_t func, void *data);
+
+extern errno_t alloc40_layout_bad(generic_entity_t *entity, 
+				  region_func_t func,
+				  void *data);
+#endif
