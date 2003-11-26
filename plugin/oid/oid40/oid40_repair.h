9a025a7a26fac82ecacb349cd4e14bbe35669901
localdomain!umka <localdomain!umka>
2003-11-26 14:44:14 +0000

    All print stuff in all plugins and library hasmoved to repair code. Related changes.
    2003/11/26 16:21:03+03:00 localdomain!umka
    
    (Logical change 1.3893)

---
+++ plugin/oid/oid40/oid40_repair.h
@@ -0,0 +1,17 @@
+/* Copyright (C) 2001, 2002, 2003 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   oid40_repair.h -- reiser4 oid allocator plugin repair functions. */
+
+#ifndef OID40_REPAIR_H
+#define OID40_REPAIR_H
+
+#ifndef ENABLE_STAND_ALONE
+#include <reiser4/plugin.h>
+
+extern errno_t oid40_print(generic_entity_t *entity,
+			   aal_stream_t *stream,
+			   uint16_t options);
+#endif
+
+#endif
