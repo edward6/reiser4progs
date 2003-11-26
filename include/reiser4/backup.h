e51999a50312cea22938ef212f105ec4f33eabef
namesys.com!vitaly <namesys.com!vitaly>
2003-11-26 10:29:44 +0000

    merge
    2003/11/26 12:20:03+03:00 localdomain!umka
    Some changes in comments format in backup.c, cleanups in measurefs.c and fixes in tree.c in tree_expand() Vitaly has discovered recently.
    
    (Logical change 1.3891)

--- include/reiser4/backup.h
+++ include/reiser4/backup.h
@@ -3,18 +3,19 @@
    
    backup.h -- reiser4 backup method declaration. */
 
-
 #ifndef REISER4_BACKUP_H
 #define REISER4_BACKUP_H
 
 #ifndef ENABLE_STAND_ALONE
 
-extern reiser4_backup_t *reiser4_backup_create(reiser4_fs_t *fs);
-extern void reiser4_backup_close(reiser4_backup_t *backup);
-extern void reiser4_backup_sync(reiser4_backup_t *backup);
-
 extern errno_t reiser4_backup_layout(reiser4_fs_t *fs,
 				     region_func_t region_func, 
 				     void *data);
+
+extern void reiser4_backup_sync(reiser4_backup_t *backup);
+extern void reiser4_backup_close(reiser4_backup_t *backup);
+
+extern reiser4_backup_t *reiser4_backup_create(reiser4_fs_t *fs);
+
 #endif
 #endif
