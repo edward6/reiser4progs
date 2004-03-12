b2e7d909112ae6610923b0ca15577f3de7729ead
namesys.com!vitaly <namesys.com!vitaly>
2004-03-12 18:26:08 +0000

    2004/03/12 21:20:45+03:00 namesys.com!vitaly Rename: plugin/item/blackbox40/blackbox40_repair.h -> plugin/item/bbox40/bbox40_repair.h
    (Logical change 1.4107)

---
+++ plugin/item/bbox40/bbox40_repair.h
@@ -0,0 +1,19 @@
+/* Copyright (C) 2001, 2002, 2003, 2004 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   bbox40_repair.h -- black box, reiser4 default safe link plugin repair method
+   declarations. */
+
+#ifndef BLACKBOX40_REPAIR_H
+#define BLACKBOX40_REPAIR_H
+#include <reiser4/plugin.h>
+
+extern reiser4_core_t *bbox40_core;
+
+extern errno_t bbox40_check_struct(reiser4_place_t *place, uint8_t mode);
+
+extern void bbox40_print(reiser4_place_t *place, 
+			 aal_stream_t *stream,
+			 uint16_t options);
+
+#endif
