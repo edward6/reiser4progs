ff983ce3e3ec3348a1cdfc70a6d603ddd8697e79
namesys.com!vitaly <namesys.com!vitaly>
2004-03-29 13:34:45 +0000

    2004/03/29 13:30:51+04:00 namesys.com!vitaly
    (Logical change 1.4124)

---
+++ plugin/object/obj40/obj40_repair.h
@@ -0,0 +1,52 @@
+/* Copyright (C) 2001, 2002, 2003, 2004 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   obj40_repair.h -- reiser4 file plugins common repair structures and methods. */
+
+#ifndef OBJ40_REPAIR_H
+#define OBJ40_REPAIR_H
+
+#include "obj40.h"
+#include "repair/plugin.h"
+
+typedef errno_t (*stat_func_t) (reiser4_place_t *);
+
+#define SKIP_METHOD	((void *)-1)
+
+struct obj40_stat_methods {
+	int (*check_mode) (obj40_t *obj, uint16_t *, uint16_t);
+	int (*check_nlink) (obj40_t *obj, uint32_t *, uint32_t);
+	int (*check_size) (obj40_t *obj, uint64_t *, uint64_t);
+	int (*check_bytes) (obj40_t *obj, uint64_t *, uint64_t);
+};
+
+typedef struct obj40_stat_methods obj40_stat_methods_t;
+
+struct obj40_stat_params {
+	uint64_t size;
+	uint64_t bytes;
+	uint32_t nlink;
+	uint16_t mode;
+};
+
+typedef struct obj40_stat_params obj40_stat_params_t;
+
+extern reiser4_plug_t *obj40_plug_recognize(obj40_t *obj, 
+					    rid_t type, 
+					    char *name);
+
+extern errno_t obj40_recognize(obj40_t *obj, stat_func_t stat_func);
+
+extern errno_t obj40_save_stat(obj40_t *obj, statdata_hint_t *hint);
+
+extern errno_t obj40_check_stat(obj40_t *obj, 
+				obj40_stat_methods_t *methods,
+				obj40_stat_params_t *params,
+				uint8_t mode);
+
+extern errno_t obj40_fix_key(obj40_t *obj, reiser4_place_t *place, 
+			     reiser4_key_t *key, uint8_t mode);
+
+extern errno_t obj40_prepare_stat(obj40_t *obj, uint16_t objmode, uint8_t mode);
+
+#endif
