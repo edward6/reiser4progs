0c9b11e86361e5ee328d810aeb3bd5dcf441944e
namesys.com!vitaly <namesys.com!vitaly>
2004-04-01 08:39:41 +0000

    2004/04/01 12:27:38+04:00 namesys.com!vitaly Rename: include/misc/param.h -> include/misc/profile.h
    (Logical change 1.4126)

---
+++ include/misc/profile.h
@@ -0,0 +1,14 @@
+/* Copyright (C) 2001, 2002, 2003, 2004 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   profile.h -- methods for working with profile reiser4 programs. */
+
+#ifndef MISC_PROFILE_H
+#define MISC_PROFILE_H
+
+#include <reiser4/types.h>
+
+extern void misc_profile_print(void);
+extern errno_t misc_profile_override(char *override);
+
+#endif
