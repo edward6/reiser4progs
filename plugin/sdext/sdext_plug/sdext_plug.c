aefe7809c1a33669b7a22b921c4bbcc15936566f
namesys.com!vitaly <namesys.com!vitaly>
2004-05-12 13:34:06 +0000

    2004/05/07 23:51:34+04:00 namesys.com!vitaly
    (Logical change 1.4136)

---
+++ plugin/sdext/sdext_plug/sdext_plug.c
@@ -0,0 +1,147 @@
+/* Copyright (C) 2001, 2002, 2003, 2004 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   sdext_plug.c -- plugin id stat data extension plugin. */
+
+#include "sdext_plug.h"
+
+reiser4_core_t *sdext_plug_core = NULL;
+
+uint16_t sdext_plug_length(stat_entity_t *stat, void *hint) {
+	uint8_t count = 0;
+	
+	/* If hint is given, count its pset. */
+	if (hint) {
+		sdhint_plug_t *h = (sdhint_plug_t *)hint;
+		uint8_t i;
+
+		for (i = 0; i < OPSET_STORE_LAST; i++)
+			count += ((aal_test_bit(&h->mask, i)) ? 1 : 0);
+	} else {
+		sdext_plug_t *plug = (sdext_plug_t *)stat_body(stat);
+		
+		count = sdext_plug_get_count(plug);
+	}
+	
+	/* Count on-disk pset. */
+	return sizeof(sdext_plug_slot_t) * count + 
+		(count ? sizeof(sdext_plug_t) : 0);	
+}
+
+static errno_t sdext_plug_open(stat_entity_t *stat, void *hint) {
+	sdhint_plug_t *plugh;
+	sdext_plug_t *ext;
+	uint16_t i;
+	
+	aal_assert("vpf-1597", stat != NULL);
+	aal_assert("vpf-1598", hint != NULL);
+
+	plugh = (sdhint_plug_t *)hint;
+	ext = (sdext_plug_t *)stat_body(stat);
+	
+	aal_memset(plugh, 0, sizeof(*plugh));
+	
+	for (i = 0; i < sdext_plug_get_count(ext); i++) {
+		rid_t mem, id;
+
+		mem = sdext_plug_get_member(ext, i);
+		id = sdext_plug_get_pid(ext, i);
+		
+		/* Check the member id valideness. */
+		if (mem >= OPSET_STORE_LAST)
+			return -EIO;
+		
+		/* Check if we met this member already. */
+		if (plugh->plug[mem])
+			return -EIO;
+
+		/* Obtain the plugin by the id. */
+		plugh->plug[mem] = sdext_plug_core->pset_ops.find(mem, id);
+		
+		if (plugh->plug[mem] == INVAL_PTR) {
+			aal_error("Failed to find a plugin of the opset "
+				  "member (%u), id (%u).", mem, id);
+			return -EIO;
+		}
+#if 0		
+		/* If the plug is valid but cannot be found (not implemented 
+		   yet), set INVAL_PTR into the opset (to let upper levels to 
+		   know that smth is stored here, but there is no such plugin). 
+		 */
+		if (!h->plug[mem])
+			h->plug[mem] = INVAL_PTR;
+#endif
+		aal_set_bit(&plugh->mask, mem);
+	}
+
+	return 0;
+}
+
+#ifndef ENABLE_STAND_ALONE
+static errno_t sdext_plug_init(stat_entity_t *stat, void *hint) {
+	sdhint_plug_t *plugh;
+	sdext_plug_t *ext;
+	uint8_t count = 0;
+	rid_t mem;
+	
+	aal_assert("vpf-1600", stat != NULL);
+	aal_assert("vpf-1599", hint != NULL);
+	
+	plugh = (sdhint_plug_t *)hint;
+	ext = (sdext_plug_t *)stat_body(stat);
+	
+	for (mem = 0; mem < OPSET_STORE_LAST; mem++) {
+		/* Find the plugin to be stored. */
+		if (!aal_test_bit(&plugh->mask, mem))
+			continue;
+
+		sdext_plug_set_member(ext, count, mem);
+		sdext_plug_set_pid(ext, count, plugh->plug[mem] ? 
+				   plugh->plug[mem]->id.id : 0);
+
+		count++;
+	}
+	
+	sdext_plug_set_count(ext, count);
+
+	return 0;
+}
+
+extern errno_t sdext_plug_check_struct(stat_entity_t *stat, 
+				       repair_hint_t *hint);
+
+extern void sdext_plug_print(stat_entity_t *stat, 
+			     aal_stream_t *stream, 
+			     uint16_t options);
+
+#endif
+
+static reiser4_sdext_ops_t sdext_plug_ops = {
+	.open	 	= sdext_plug_open,
+#ifndef ENABLE_STAND_ALONE
+	.init	 	= sdext_plug_init,
+	.print   	= sdext_plug_print,
+	.check_struct   = sdext_plug_check_struct,
+#endif
+	.length	 	= sdext_plug_length
+};
+
+static reiser4_plug_t sdext_plug_plug = {
+	.cl    = class_init,
+	.id    = {SDEXT_PLUG_ID, 0, SDEXT_PLUG_TYPE},
+#ifndef ENABLE_STAND_ALONE
+	.label = "sdext_plug",
+	.desc  = "Plugin id stat data extension for reiser4, ver. " VERSION,
+#endif
+	.o = {
+		.sdext_ops = &sdext_plug_ops
+	}
+};
+
+static reiser4_plug_t *sdext_plug_start(reiser4_core_t *c) {
+	sdext_plug_core = c;
+	return &sdext_plug_plug;
+}
+
+plug_register(sdext_plug, sdext_plug_start, NULL);
+
---
+++ plugin/sdext/sdext_plug/sdext_plug_repair.c
@@ -0,0 +1,167 @@
+/* Copyright (C) 2001, 2002, 2003, 2004 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   sdext_lt_repair.c -- large time stat data extension plugin recovery code. */
+
+#ifndef ENABLE_STAND_ALONE
+#include "sdext_plug.h"
+#include <repair/plugin.h>
+
+char *opset_name[OPSET_STORE_LAST] = {
+	[OPSET_OBJ] =	  "object    ",
+	[OPSET_DIR] =	  "directory ",
+	[OPSET_PERM] =	  "permission",
+	[OPSET_POLICY] =  "formatting",
+	[OPSET_HASH] =	  "hash      ",
+	[OPSET_FIBRE] =   "fibration ",
+	[OPSET_STAT] =	  "statdata  ",
+	[OPSET_DIRITEM] = "diritem   ",
+	[OPSET_CRYPTO] =  "crypto    ",
+	[OPSET_DIGEST] =  "digest    ",
+	[OPSET_COMPRES] = "compress  "
+};
+
+errno_t sdext_plug_check_struct(stat_entity_t *stat, repair_hint_t *hint) {
+	sdhint_plug_t plugh;
+	sdext_plug_t *ext;
+	uint64_t mask = 0;
+	int8_t i, count;
+	int8_t remove;
+	uint32_t len;
+	void *dst;
+	
+	ext = (sdext_plug_t *)stat_body(stat);
+	count = sdext_plug_get_count(ext);
+	len = sdext_plug_length(stat, NULL);
+	
+	if (stat->offset + len < stat->place->len) {
+		aal_error("Node (%llu), item (%u): does not look like a "
+			  "valid plug extention: wrong count of plugins "
+			  "detected (%u).", place_blknr(stat->place),
+			  stat->place->pos.item, count);
+		return RE_FATAL;
+	}
+	    
+	aal_memset(&plugh, 0, sizeof(plugh));
+	remove = 0;
+	
+	for (i = 0; i < count; i++) {
+		rid_t mem, id;
+
+		mem = sdext_plug_get_member(ext, i);
+		id = sdext_plug_get_pid(ext, i);
+
+		if (mem >= OPSET_STORE_LAST) {
+			/* Unknown member. */
+			aal_error("Node (%llu), item (%u): the slot (%u) "
+				  "contains the invalid opset member (%u).",
+				  place_blknr(stat->place), 
+				  stat->place->pos.item, i, mem);
+
+			aal_set_bit(&mask, i);
+			remove++;
+		} else if (plugh.plug[mem]) {
+			/* Was met already. */
+			aal_error("Node (%llu), item (%u): the slot (%u) "
+				  "contains the opset member (%s) that was "
+				  "met already.", place_blknr(stat->place), 
+				  stat->place->pos.item, i, opset_name[mem]);
+
+			aal_set_bit(&mask, i);
+			remove++;
+		} else {
+			/* Obtain the plugin. */
+			plugh.plug[mem] = 
+				sdext_plug_core->pset_ops.find(mem, id);
+
+			/* Check if the member is valid. */
+			if (plugh.plug[mem] == INVAL_PTR) {
+				aal_error("Node (%llu), item (%u): the slot "
+					  "(%u) contains the invalid opset "
+					  "member (%s), id (%u).",
+					  place_blknr(stat->place), 
+					  stat->place->pos.item, i, 
+					  opset_name[mem], id);
+				
+				aal_set_bit(&mask, i);
+				remove++;
+			} else if (!plugh.plug[mem]) {
+				/* For those members where no one plugin is 
+				   written, set INVAL_PTR to avoid meeting it 
+				   another time. */
+				plugh.plug[mem] = INVAL_PTR;
+			}
+		}
+	}
+
+	if (!mask) 
+		return 0;
+	
+	/* Some broken slots are found. */
+	if (hint->mode != RM_BUILD)
+		return RE_FATAL;
+
+	if (remove == count) {
+		aal_error("Node (%llu), item (%u): no slot left. Does "
+			  "not look like a valid (%s) statdata extention.",
+			  place_blknr(stat->place), stat->place->pos.item,
+			  stat->ext_plug->label);
+		return RE_FATAL;
+	}
+	
+	/* Removing broken slots. */
+	aal_error("Node (%llu), item (%u): removing broken slots.",
+		  place_blknr(stat->place), stat->place->pos.item);
+	
+	dst = stat_body(stat) + sizeof(sdext_plug_t) + 
+		(count - 1) * (sizeof(sdext_plug_slot_t));
+	
+	for (i = count - 1; i >= 0; i--, dst -= sizeof(sdext_plug_slot_t)) {
+		if (!aal_test_bit(&mask, i))
+			continue;
+
+		aal_memmove(dst, dst + sizeof(sdext_plug_slot_t),
+			    len - (dst - (void *)stat_body(stat)) - 
+			    sizeof(sdext_plug_slot_t));
+	}
+	
+	hint->len = remove * sizeof(sdext_plug_slot_t);
+	
+	return 0;
+}
+
+void sdext_plug_print(stat_entity_t *stat, 
+		      aal_stream_t *stream, 
+		      uint16_t options) 
+{
+	reiser4_plug_t *plug;
+	sdext_plug_t *ext;
+	uint16_t i;
+
+	aal_assert("vpf-1603", ext != NULL);
+	aal_assert("vpf-1604", stream != NULL);
+	
+	ext = (sdext_plug_t *)stat_body(stat);
+
+	aal_stream_format(stream, "Pset count: \t%u\n",
+			  sdext_plug_get_count(ext));
+
+	for (i = 0; i < sdext_plug_get_count(ext); i++) {
+		rid_t mem, id;
+
+		mem = sdext_plug_get_member(ext, i);
+		id = sdext_plug_get_pid(ext, i);
+		
+		plug = sdext_plug_core->pset_ops.find(mem, id);
+
+		aal_stream_format(stream, "    %s : id = %u",
+				  opset_name[mem], id);
+
+		if (plug && plug != INVAL_PTR) 
+			aal_stream_format(stream, " (%s)\n", plug->label);
+		else
+			aal_stream_format(stream, "\n");
+	}
+}
+
+#endif
