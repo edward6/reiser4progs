7b0f32c97a99f037a18fc9805b624b97fe60adb6
namesys.com!vitaly <namesys.com!vitaly>
2004-06-22 18:36:40 +0000

    (Logical change 1.4172)

---
+++ libaux/gauge.c
@@ -0,0 +1,22 @@
+/* Copyright (C) 2001, 2002, 2003, 2004 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+
+   gauge.c -- common for all progs gauge metods. */
+
+#include <aal/aal.h>
+#include <aux/gauge.h>
+
+aal_gauge_handler_t aux_gauge_handlers[GT_LAST];
+
+void aux_gauge_set_handler(aal_gauge_handler_t handler,
+			   aux_gauge_type_t type)
+{
+	aal_assert("vpf-1685", type < GT_LAST);
+	aux_gauge_handlers[type] = handler;
+}
+
+aal_gauge_handler_t aux_gauge_get_handler(aux_gauge_type_t type) {
+	aal_assert("vpf-1685", type < GT_LAST);
+	return aux_gauge_handlers[type];
+}
+
