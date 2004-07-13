589cb2d763f78ac9474b9f2ac3d8fb728c7d849f
namesys.com!vitaly <namesys.com!vitaly>
2004-07-13 12:07:45 +0000

    (Logical change 1.4184)

--- libaux/gauge.c
+++ libaux/gauge.c
@@ -4,7 +4,7 @@
    gauge.c -- common for all progs gauge metods. */
 
 #ifndef ENABLE_STAND_ALONE
-#include <aal/aal.h>
+#include <aal/libaal.h>
 #include <aux/gauge.h>
 
 aal_gauge_handler_t aux_gauge_handlers[GT_LAST];
