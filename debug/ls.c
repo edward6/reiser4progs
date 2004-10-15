25f58507d12ae6b37760f9fdbc047bc62aa512fb
namesys.com!vitaly <namesys.com!vitaly>
2004-10-15 21:11:06 +0000

    2004/10/15 22:24:54+04:00 namesys.com!vitaly Rename: demos/ls.c -> debug/ls.c
    (Logical change 1.4224)

---
+++ debug/ls.c
@@ -0,0 +1,46 @@
+/* Copyright (C) 2001, 2002, 2003, 2004 by Hans Reiser, licensing governed by
+   reiser4progs/COPYING.
+   
+   ls.c -- a demo program which works similar like to standard ls utility. */
+
+#include "busy.h"
+
+errno_t ls_cmd(busy_ctx_t *ctx) {
+	reiser4_object_t *object;
+	entry_hint_t entry;
+
+	aal_assert("vpf-1710", ctx != NULL);
+
+	if (!ctx->in.fs) {
+		aal_error("Fs is not openned. Wrong PAth is specified: %s.",
+			  ctx->in.path);
+		return -EINVAL;
+	}
+	
+	if (ctx->in.path[0] == 0) {
+		aal_error("NULL path is given.");
+		return -EINVAL;
+	}
+	
+	if (!(object = reiser4_semantic_open(ctx->in.fs->tree, 
+					     ctx->in.path, NULL, 1))) {
+		aal_error("Can't open file %s.", ctx->in.path);
+		return -EIO;
+	}
+
+	if (object->ent->opset.plug[OPSET_OBJ]->id.group == DIR_OBJECT) {
+		while (reiser4_object_readdir(object, &entry) > 0) {
+			printf("[%s] %s\n", 
+			       reiser4_print_key(&entry.object, PO_DEFAULT), 
+			       entry.name);
+		}
+	} else {
+		printf("[%s] %s\n", 
+		       reiser4_print_key(&object->ent->object, PO_DEFAULT), 
+		       ctx->in.path);
+	}
+	
+	reiser4_object_close(object);
+	return 0;
+}
+
